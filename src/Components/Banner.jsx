f58f164812197242698a82d0742b7b5e044b5145
pandeynehagit <nleetcode@gmail.com>
2025-02-16 12:11:27 +0530

    file added to gitignore

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,48 +1,3 @@
-// import React, { useState, useEffect, useContext } from "react";
-// import { MoviesContext } from "../Context/MoviesContext";
-
-// const Banner = () => {
-//   const { movies } = useContext(MoviesContext);
-//   const [bannerImage, setBannerImage] = useState("");
-//   const [title, setTitle] = useState("PlaceHolder");
-//   useEffect(() => {
-//     if (movies.length > 0) {
-//       const idx = Math.floor(Math.random() * movies.length);
-//       const selectedMovie = movies[idx];
-
-//       // Ensure selectedMovie has necessary properties
-//       if (selectedMovie && selectedMovie.backdrop_path) {
-//         // setBannerImage(
-//         //   "https://image.tmdb.org/t/p/original/wNAhuOZ3Zf84jCIlrcI6JhgmY5q.jpg"
-//         // );
-//         setBannerImage(
-//           `https://image.tmdb.org/t/p/original/${selectedMovie.backdrop_path}`
-//         );
-//         setTitle(selectedMovie.title);
-//       } else {
-//         setBannerImage("https://picsum.photos/seed/picsum/200/300");
-//         //     setTitle("Placeholder"); // Default title if not available
-//         //   }
-//         // } else {
-//         //   setBannerImage(""); // Reset banner image if movies or results are not available
-//         //   setTitle("Placeholder"); // Reset title if movies or results are not available
-//       }
-//     }
-//   }, [movies]);
-
-//   return (
-//     <div
-//       className="h-[20vh] md:h-[50vh] bg-top bg-cover bg-center flex items-end"
-//       style={{
-//         backgroundImage: `url(${bannerImage})`,
-//       }}
-//     >
-//       <div className="text-white w-full text-center text-2xl">{title}</div>
-//     </div>
-//   );
-// };
-
-// export default Banner;
 import { useContext, useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { MoviesContext } from "../Context/MoviesContext"; // Adjust the path as needed
@@ -70,7 +25,8 @@ const Banner = () => {
   useEffect(() => {
     const timer = setInterval(nextSlide, 5000);
     return () => clearInterval(timer);
-  }, [latestMovies.length]);
+  }, [nextSlide, latestMovies.length]);
+  //error handling
   if (latestMovies.length === 0) {
     return (
       <div className="w-full h-[30vh] md:h-[50vh] bg-gray-200 flex items-center justify-center">
